c44b14a752a3d4e9d64fc23047f70fdd2d9576dd
Yevhen Shymotyuk <fly1ngdream@pm.me>
2020-06-26 17:41:31 +0300

    Add documentation for config package

--- config/config.go
+++ config/config.go
@@ -7,6 +7,7 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Config represents config data that is stored in the file
 type Config struct {
 	Dest     string            `toml:"dest"`
 	Emoticon map[string]string `toml:"emoticon"`
@@ -18,6 +19,7 @@ func (e *configAlreadyExistsError) Error() string {
 	return "config file already exists"
 }
 
+// Load unmarshals config data to Config object
 func Load(v *viper.Viper) (*Config, error) {
 	err := v.ReadInConfig()
 	if err != nil {
@@ -38,6 +40,7 @@ func Load(v *viper.Viper) (*Config, error) {
 	return c, err
 }
 
+// Create creates config file using defaults from Config object
 func Create(v *viper.Viper, c Config) (*Config, error) {
 	if err := v.ReadInConfig(); err == nil {
 		return nil, &configAlreadyExistsError{}
@@ -62,6 +65,7 @@ func Create(v *viper.Viper, c Config) (*Config, error) {
 	return &c, nil
 }
 
+// File returns path to the config file
 func File(v *viper.Viper) string {
 	return v.ConfigFileUsed()
 }
